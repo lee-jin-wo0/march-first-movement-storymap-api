fa2c33a440c8379c35f6f8d02fa0dfd55ccba324
lee-jin-wo0 <leejinwo0@mail.hongik.ac.kr>
2026-08-05 09:59:06 +0900

    feat(20260805): add various seoul base map endpoints and split map filter css

--- api/mapService.js
+++ api/mapService.js
@@ -2,8 +2,14 @@
    API 엔드포인트(URL) 관리 객체
 ======================================================= */
 export const MAP_ENDPOINTS = {
+  seoulBaseMap_normal: `https://map.seoul.go.kr/openapi/v5/${CONFIG.MAP_API_KEY}/public/map/base/dawul_normal/{z}/{j}/{k}/{x}/{y}/png`,
+  seoulBaseMap_air: `https://map.seoul.go.kr/openapi/v5/${CONFIG.MAP_API_KEY}/public/map/base/dawul_air/{z}/{j}/{k}/{x}/{y}/png`,
+  seoulBaseMap_onlyair: `https://map.seoul.go.kr/openapi/v5/${CONFIG.MAP_API_KEY}/public/map/base/dawul_onlyair/{z}/{j}/{k}/{x}/{y}/png`,
   seoulBaseMap_kor: `https://map.seoul.go.kr/openapi/v5/${CONFIG.MAP_API_KEY}/public/map/base/dawul_kor_normal/{z}/{j}/{k}/{x}/{y}/png`,
+  seoulBaseMap_big_kor: `https://map.seoul.go.kr/openapi/v5/${CONFIG.MAP_API_KEY}/public/map/base/dawul_kor_bigfontnormal/{z}/{j}/{k}/{x}/{y}/png`,
+  seoulBaseMap_kor_air: `https://map.seoul.go.kr/openapi/v5/${CONFIG.MAP_API_KEY}/public/map/base/dawul_kor_air/{z}/{j}/{k}/{x}/{y}/png`,
   seoulBaseMap_eng: `https://map.seoul.go.kr/openapi/v5/${CONFIG.MAP_API_KEY}/public/map/base/dawul_eng_normal/{z}/{j}/{k}/{x}/{y}/png`,
+  seoulBaseMap_eng_air: `https://map.seoul.go.kr/openapi/v5/${CONFIG.MAP_API_KEY}/public/map/base/dawul_eng_air/{z}/{j}/{k}/{x}/{y}/png`,
   themeData_11100550: `https://map.seoul.go.kr/openapi/v5/${CONFIG.THEMA_API_KEY}/public/themes/contents/ko?page_size=999&page_no=1&coord_x=126.974695&coord_y=37.564150&distance=999999&search_type=0&search_name=&theme_id=11100550&content_id=&subcate_id=`,
   themeData_100173: `https://map.seoul.go.kr/openapi/v5/${CONFIG.THEMA_API_KEY}/public/themes/contents/ko?page_size=999&page_no=1&coord_x=126.974695&coord_y=37.564150&distance=999999&search_type=0&search_name=&theme_id=100173&content_id=&subcate_id=`
 };
